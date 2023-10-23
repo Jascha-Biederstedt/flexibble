016e670ccde7019b94a2a9a4f835910491aa1a39
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-23 18:46:11 +0200

    Add nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import { NavLinks } from '@/constants';
+
 const Navbar = () => {
   return (
-    <nav className='flex-between navbar'>
-      <div className='flex-1 flex-start gap-10'>
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
         <Link href='/'>
           <Image src='/logo.svg' alt='Flexibble Logo' width={115} height={43} />
         </Link>
+
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map(link => (
+            <Link key={link.key} href={link.href}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
       </div>
     </nav>
   );
