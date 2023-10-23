a73a105285f181e3b1b036c5c39e02db9276ed2d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-23 18:53:09 +0200

    Add auth providers comp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { NavLinks } from '@/constants';
+import AuthProviders from './AuthProviders';
 
 const Navbar = () => {
   const session = {};
