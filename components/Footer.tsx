eaa6505138639bf61c47f41cc86ea0688364964e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-24 18:46:45 +0200

    Add footer logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Image
+            src='/logo-purple.svg'
+            width={115}
+            height={38}
+            alt='Flexibble Footer Logo'
+          />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
