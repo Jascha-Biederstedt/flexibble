f494ace53e7061921db94e15803add4c7d1d9a94
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-24 18:50:48 +0200

    Add footer column comp

---
+++ components/FooterColumn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterColumn = () => {
+  return (
+    <div className='footer_column'>
+      <h4 className='font-semibold'>
+        <ul className='flex flex-col gap-2 font-normal'>Links</ul>
+      </h4>
+    </div>
+  );
+};
+
+export default FooterColumn;
