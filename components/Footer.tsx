b6536343efa4bd0904d2f00c1dc48d734d94c42c
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-26 13:02:25 +0200

    Add footer project count

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,6 +63,10 @@ const Footer = () => {
 
       <div className='flexBetween footer_copyright'>
         <p>@2023 Flexibble. All rights reserved.</p>
+        <p className='text-gray'>
+          <span className='text-black font-semibold'>10,214</span> projects
+          submitted
+        </p>
       </div>
     </footer>
   );
