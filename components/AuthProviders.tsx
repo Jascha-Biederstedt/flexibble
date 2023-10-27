0385d436801261d2b0775a58e3b5b2b4414d0041
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-27 11:10:52 +0200

    Add fetch provider fn in auth providers comp

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+'use client';
+
+import { get } from 'http';
+import { getProviders, signIn } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+
+type Provider = {
+  id: String;
+  name: String;
+  type: String;
+  signinUrl: String;
+  callbackUrl: String;
+  signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, index) => (
+          <button key={index}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
